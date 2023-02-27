8fb3417995a72f339a95d78765a3525ffc3da25a
Aneet Mandal <aneet.mandal@gmail.com>
2023-02-28 02:12:17 +0530

    Added The Latest Cards

--- reactblogfrontend/src/components/home.js
+++ reactblogfrontend/src/components/home.js
@@ -11,9 +11,60 @@ return(
         <span id="TitleFont">
             Title of vertical gallery
         </span>
+        <span id="SubTitleFont">
+        Travel / February 27 2023
+        </span>
     </div>
-    <div class="Img2"></div>
-    <div class="Img3"></div>
+    <div class="Img2">
+    <span id="Img2Font">
+            The Sound Cloud<br /> you loved is doomed
+        </span>
+        <span id="Img2SubFont">
+        Travel / February 27 2023
+        </span>
+    </div>
+    <div class="Img3">
+    <span id="Img3Font">
+            The Sound Cloud<br /> you loved is doomed
+        </span>
+        <span id="Img3SubFont">
+        Travel / February 27 2023
+        </span>
+    </div>
+
+    <div class="Latest">The Latest</div>
+    <div class="Latest-line"></div>
+
+    <div class="LatestCard-1">
+        <div class="Temple-1"></div>
+        <div class="Temple-1-Text">Joshua Tree <br /> Overnight Adventure</div>
+        <div class="Temple-2-Text">Gujarat is vastly underrated and it’s a mystery
+         to us why the region isn’t more well-known as a tourist destination. 
+         It has a plethora of temples and palaces</div>
+        <div class="Temple-Footer">Travel</div> 
+        <div class="Temple-Footer-2">/ February 27 2023</div> 
+    </div>
+
+    <div class="LatestCard-2">
+        <div class="Temple-2"></div>
+        <div class="Temple2-2-Text">Joshua Tree <br /> Overnight Adventure</div>
+        <div class="Temple-22-Text">Gujarat is vastly underrated and it’s a mystery
+         to us why the region isn’t more well-known as a tourist destination. 
+         It has a plethora of temples and palaces</div>
+        <div class="Temple2-Footer">Travel</div> 
+        <div class="Temple2-Footer-2">/ February 27 2023</div> 
+    </div>
+
+    <div class="LatestCard-3">
+    <div class="Temple-3"></div>
+    <div class="Temple3-Text">Joshua Tree <br /> Overnight Adventure</div>
+        <div class="Temple-33-Text">Gujarat is vastly underrated and it’s a mystery
+         to us why the region isn’t more well-known as a tourist destination. 
+         It has a plethora of temples and palaces</div>
+         <div class="Temple3-Footer">Travel</div> 
+        <div class="Temple3-Footer-3">/ February 27 2023</div> 
+    </div>
+
     </div>
     </>
 );
